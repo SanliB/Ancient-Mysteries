3c83b54fadf107f5bb733c85fec88c7e276b1e54
Ömer Abuzer Sarıkaş <omer.97.63.gs@gmail.com>
2024-03-25 23:32:08 +0300

    Add Anubis

---
+++ Assets/_Project/Runtime/Project/Launcher/Scripts/Vo/AnubisPuzzle.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class AnubisPuzzle : MonoBehaviour
+{
+    private Vector3 _catStatus;
+    public Vector3 TrueRot;
+    public bool _trueRotStatus=false;
+    public GameObject[] CatList;
+    public int sayac=0;
+    public bool status=false;
+    GameObject door;
+    float a;
+    
+    void Awake(){
+        door= DoorController.Instance.Door(0);
+        a=door.transform.position.y+4;
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log(gameObject.tag);
+        if(gameObject.tag=="Cat"){
+            SoundManager.Instance.Audio(0);
+            transform.Rotate(0,45,0);
+            _catStatus=transform.rotation.eulerAngles;
+            if(TrueRot.y <= _catStatus.y &&  _catStatus.y<=TrueRot.y+1){
+                _trueRotStatus=true;
+            }
+            else{
+                _trueRotStatus=false;
+            }
+        }
+        if(gameObject.tag=="Anubis"){
+            sayac=0;
+            for(int i=0; i<CatList.Length;i++){
+                if(CatList[i].GetComponent<AnubisPuzzle>()._trueRotStatus){
+                    sayac++;
+                }
+            }
+            if(sayac==CatList.Length && status==false){
+                status=true;
+                SoundManager.Instance.Audio(1);
+                door.transform.position=new Vector3(door.transform.position.x,a,door.transform.position.z);
+                Debug.Log("kapı açıldı");
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Runtime/Project/Launcher/Scripts/Vo/WallPuzzle.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallPuzzle : MonoBehaviour
+{
+    private Vector3 _buttonStatus;
+    public Vector3 TrueRot;
+    public bool _trueRotStatus=false;
+    public GameObject[] ButtonList;
+    public int sayac=0;
+    public bool status=false;
+
+    private void OnMouseDown()
+    {
+        Debug.Log(gameObject.tag);
+        if(gameObject.tag=="WallKey"){
+            SoundManager.Instance.Audio(0);
+            transform.Rotate(0,0,72);
+            _buttonStatus=transform.rotation.eulerAngles;
+            if(TrueRot.y <= _buttonStatus.y &&  _buttonStatus.y<=TrueRot.y+1){
+                _trueRotStatus=true;
+            }
+            else{
+                _trueRotStatus=false;
+            }
+        }
+        if(gameObject.tag=="WallPuzzleButton"){
+            sayac=0;
+            for(int i=0; i<ButtonList.Length;i++){
+                if(ButtonList[i].GetComponent<WallPuzzle>()._trueRotStatus){
+                    sayac++;
+                }
+            }
+            if(sayac==ButtonList.Length && status==false){
+                status=true;
+                SoundManager.Instance.Audio(1);
+                //door.transform.position=new Vector3(door.transform.position.x,a,door.transform.position.z);
+                Debug.Log("kapı açıldı");
+            }
+        }
+    }
+}
