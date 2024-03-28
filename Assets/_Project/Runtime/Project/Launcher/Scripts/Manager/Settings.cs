0020f1c69fc9c0b15ef7db886d2e0d7ed47dc5ca
Betül Şanlıbayrak <betulsanlibayrak@gmail.com>
2024-03-29 01:02:32 +0300

    puzzle fixed and effect setting added.

---
+++ Assets/_Project/Runtime/Project/Launcher/Scripts/Manager/Settings.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public Slider Sounds;
+    public AudioSource Audio;
+
+    public void GetAudioVolume()
+    {
+        Audio.volume = Sounds.value;
+        Audio.Play();
+
+    }
+
+    private void Start()
+    {
+        PlayerPrefs.SetFloat("audioVolume", Audio.volume);
+    }
+
+}
