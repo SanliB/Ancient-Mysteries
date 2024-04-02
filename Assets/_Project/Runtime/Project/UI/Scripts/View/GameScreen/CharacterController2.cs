3b661b610008705efba65763ccd626f1f22e91a8
AysenurMadan <nuranmadan@gmail.com>
2024-04-02 16:24:50 +0300

    Walk Sound Added

--- Assets/_Project/Runtime/Project/UI/Scripts/View/GameScreen/CharacterController2.cs
+++ Assets/_Project/Runtime/Project/UI/Scripts/View/GameScreen/CharacterController2.cs
@@ -18,9 +18,15 @@ public class CharacterController2 : MonoSingleton<CharacterController2>
     public float Mass=1f;
     Vector3 move = Vector3.zero;
     public Transform Cam;
+    public AudioSource WalkSound;
+    private Vector3 previousMove;
+    private bool PlaySound;
+    private float WalkSoundVolume;
 
     void Start()
     {
+        WalkSoundVolume = WalkSound.volume;
+        PlaySound = false;
         _cc = GetComponent<CharacterController>();
     }
 
@@ -45,7 +51,36 @@ public class CharacterController2 : MonoSingleton<CharacterController2>
         }
     }
 
+    IEnumerator WalkSoundPlayer()
+    {
+        WalkSound.Play();
+        yield return new WaitForSeconds(11f);
+        PlaySound = false;
+    }
+
+    IEnumerator StopSound()
+    {
+        while (WalkSound.volume > 0)
+        {
+            WalkSound.volume -= 0.1f;
+            yield return null;
+        }
+        WalkSound.Stop();
+    }
+
     private void MoveCharacter(){
+        if (_horizontal != 0 && _vertical != 0 && PlaySound == false)
+        {
+            PlaySound = true;
+            WalkSound.volume = WalkSoundVolume;
+            // WalkSound.pitch = Time.deltaTime*speed;
+            StartCoroutine(WalkSoundPlayer());
+        }
+        if (_horizontal == 0 && _vertical == 0 && PlaySound == true)
+        {
+            StartCoroutine(StopSound());
+            PlaySound = false;
+        }
         move=new Vector3(_horizontal,velocity,_vertical)*Time.deltaTime*speed;
         _cc.Move(transform.TransformDirection(move));
     }
