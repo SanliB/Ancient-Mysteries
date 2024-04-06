1452fcfcc2a92bd4e9d9c44fe3471e3521c7be65
Berkay Mustafaoğlu <berkaymustafaoglu@hotmail.com>
2024-04-06 16:24:04 +0300

    VolumeAdded

---
+++ Assets/_Project/Runtime/Project/UI/Scripts/View/GameScreen/OpenningPanel.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+public class OpenningPanel : MonoBehaviour
+{
+    public Volume OpenVolume;
+    private Vignette _vg;
+    public bool FinalStatus;
+
+    public void Start()
+    {   
+        OpenVolume.profile.TryGet(out _vg);
+        FinalStatus=true;
+    }
+
+    void Update()
+    {
+        if (FinalStatus && _vg.intensity.value <= 1f)
+        {
+            _vg.intensity.value -= 0.0005f*Time.fixedTime;
+        }
+    }
+}
